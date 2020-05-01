7f8c303d9d9f0c729634600beff87f9f30fd07d7
AndZeka <andizeka@hotmail.com>
2020-05-01 19:02:05 +0200

    last commit

---
+++ main.js
@@ -0,0 +1,8 @@
+function myFunction1() {
+    document.getElementById("registerDiv").style.display = "none";
+    document.getElementById("loginDiv").style.display = "block";
+}
+function myFunction2() {
+    document.getElementById("registerDiv").style.display = "block";
+    document.getElementById("loginDiv").style.display = "none";
+}
